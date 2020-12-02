f1980f2a025d7b5c134e909b65754f4ceaefc209
tiecheng <tc@tiechengdeMacBook-Pro-2.local>
2020-12-02 19:56:06 +0800

    update

---
+++ dubbo2.7.7/src/main/java/com/funnycode/Provider.java
@@ -0,0 +1,54 @@
+package com.funnycode;
+
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ProtocolConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.ServiceConfig;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ */
+public class Provider {
+
+    public static void main(String[] args) throws IOException {
+        // 服务实现
+        DemoService demoService = new DemoServiceImpl();
+
+        // 当前应用配置
+        ApplicationConfig application = new ApplicationConfig();
+        application.setName("demoProvider");
+
+        // 连接注册中心配置
+        RegistryConfig registry = new RegistryConfig();
+        registry.setAddress("127.0.0.1:2181");
+        registry.setProtocol("zookeeper");
+        registry.setUsername("");
+        registry.setPassword("");
+
+        // 服务提供者协议配置
+        ProtocolConfig protocol = new ProtocolConfig();
+        protocol.setName("dubbo");
+        protocol.setPort(12345);
+        protocol.setThreads(200);
+
+        // 注意：ServiceConfig为重对象，内部封装了与注册中心的连接，以及开启服务端口
+
+        // 服务提供者暴露服务配置
+        ServiceConfig<DemoService> service = new ServiceConfig<>(); // 此实例很重，封装了与注册中心的连接，请自行缓存，否则可能造成内存和连接泄漏
+        service.setApplication(application);
+        service.setRegistry(registry); // 多个注册中心可以用setRegistries()
+        service.setProtocol(protocol); // 多个协议可以用setProtocols()
+        service.setInterface(DemoService.class);
+        service.setRef(demoService);
+        service.setVersion("1.0.0");
+        service.setGroup("tc");
+        service.setTimeout(60 * 1000);
+
+        // 暴露及注册服务
+        service.export();
+
+        System.in.read();
+    }
+}
